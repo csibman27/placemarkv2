fa0ef42abffb2aefb32791e60ce15055f5db44b4
tibor molnar <tmolnair@gmail.com>
2023-05-12 22:48:07 +0100

    deployment to render fixed

--- src/server.js
+++ src/server.js
@@ -55,64 +55,63 @@ const server = Hapi.server({
   port: process.env.PORT || 3000,
 });
 
-  await server.register(Inert);
-  await server.register(Vision);
-  await server.register(Cookie);
-  await server.register(jwt);
-  await server.register(Bell);
+await server.register(Inert);
+await server.register(Vision);
+await server.register(Cookie);
+await server.register(jwt);
+await server.register(Bell);
 
-  await server.register([
-    Inert,
-    Vision,
-    {
-      plugin: HapiSwagger,
-      options: swaggerOptions,
-    },
-  ]);
+await server.register([
+  Inert,
+  Vision,
+  {
+    plugin: HapiSwagger,
+    options: swaggerOptions,
+  },
+]);
 
-  server.validator(Joi);
+server.validator(Joi);
 
-  server.views({
-    engines: {
-      hbs: Handlebars,
-    },
-    relativeTo: __dirname,
-    path: "./views",
-    layoutPath: "./views/layouts",
-    partialsPath: "./views/partials",
-    layout: true,
-    isCached: false,
-  });
+server.views({
+  engines: {
+    hbs: Handlebars,
+  },
+  relativeTo: __dirname,
+  path: "./views",
+  layoutPath: "./views/layouts",
+  partialsPath: "./views/partials",
+  layout: true,
+  isCached: false,
+});
 
-  server.auth.strategy("session", "cookie", {
-    cookie: {
-      name: process.env.cookie_name,
-      password: process.env.cookie_password,
-      isSecure: false,
-    },
-    redirectTo: "/",
-    validate: accountsController.validate,
-  });
-  server.auth.strategy("jwt", "jwt", {
-    key: process.env.cookie_password,
-    validate: validate,
-    verifyOptions: { algorithms: ["HS256"] },
-  });
-  server.auth.strategy("github", "bell", {
-    provider: "github",
-    password: process.env.github_encryption_password,
+server.auth.strategy("session", "cookie", {
+  cookie: {
+    name: process.env.cookie_name,
+    password: process.env.cookie_password,
     isSecure: false,
-    clientId: process.env.github_client_id,
-    clientSecret: process.env.github_secret,
-  });
-  server.auth.default("session");
+  },
+  redirectTo: "/",
+  validate: accountsController.validate,
+});
+server.auth.strategy("jwt", "jwt", {
+  key: process.env.cookie_password,
+  validate: validate,
+  verifyOptions: { algorithms: ["HS256"] },
+});
+server.auth.strategy("github", "bell", {
+  provider: "github",
+  password: process.env.github_encryption_password,
+  isSecure: false,
+  clientId: process.env.github_client_id,
+  clientSecret: process.env.github_secret,
+});
+server.auth.default("session");
 
-  db.init("mongo");
-  server.route(webRoutes);
-  server.route(apiRoutes);
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
-}
+db.init("mongo");
+server.route(webRoutes);
+server.route(apiRoutes);
+await server.start();
+console.log("Server running on %s", server.info.uri);
 
 process.on("unhandledRejection", (err) => {
   console.log(err);
